3483f7a4db2203f4a7bb69b33883ba0ed9212d2f
John Alberto López Hernández <johnalbh@gmail.com>
2021-11-22 19:53:01 +0100

    Fix url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   STORAGE: 'Rra5BCQfZsXW8TtJ',
-  pathContribuyente: `http://localhost:8090/api/contribuyentes`,
-  pathImpuesto: `http://localhost:8090/api/impuestos`,
-  pathBienes: `http://localhost:8090/api/bienes`,
-  pathSeguridad: `http://localhost:8090/api/security`,
-  pathUsuarios: `http://localhost:8090/api/usuarios`,
+  pathContribuyente: `https://servicio-contribuyente-mimpuesto.azurewebsites.net`,
+  pathImpuesto: `https://servicio-impuesto-mimpuesto.azurewebsites.net`,
+  pathBienes: `https://servicio-bien-mimpuesto.azurewebsites.net`,
+  pathSeguridad: `https://servicio-securiy-mimpuesto.azurewebsites.net`,
+  pathUsuarios: `https://servicio-usuario-mimpuesto.azurewebsites.net`,
 };
 
 /*
